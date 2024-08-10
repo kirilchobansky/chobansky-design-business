fe62601fae65c73587cc04646e3a6bac4537a72e
kirilchobansky <153956572+kirilchobansky@users.noreply.github.com>
2024-08-10 21:51:46 +0300

    Remove saved passord in auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -6,10 +6,10 @@ export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const { password: _, ...authData } = await login(email, password);
 
-        changeAuthState(result);
-        return result;
+        changeAuthState(authData);
+        return authData;
     }
 
     return loginHandler;
@@ -19,10 +19,10 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const { password: _, ...authData } = await register(email, password);
 
-        changeAuthState(result);
-        return result;
+        changeAuthState(authData);
+        return authData;
     }
 
     return registerHandler;
